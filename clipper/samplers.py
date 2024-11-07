d14ffbac686fca941fb63d660837779ef6b6452f
Salar Ghaffarian <salarghaffarian1363@gmail.com>
2024-11-06 19:15:15 -0700

    make initial empty samplers.py file

---
+++ clipper/samplers.py
@@ -0,0 +1,4 @@
+'''
+This module will be updated with the SlideWindowSampler and VectorSampler classs, which will be used to provide the clipped/cropped samples needed for training DL models.
+
+'''
