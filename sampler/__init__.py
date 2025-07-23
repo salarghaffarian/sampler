046b185776f9150590486ef807f2623fae299f04
Salar Ghaffarian <salarghaffarian1363@gmail.com>
2025-07-23 04:07:14 -0600

    Fix an import issue in the __init__.py

--- sampler/__init__.py
+++ sampler/__init__.py
@@ -1,6 +1,6 @@
 # Import key classes or functions from clippers.py and samplers.py
 from .clippers import RasterClipper, VectorClipper
-from .samplers import SlidingWindowSampler, VectorSampler
+from .samplers import SamplingWindowGenerator, BaseSampler, Sampler
 
 # Define the __all__ variable to let them imported directly from the package.
-__all__ = ["RasterClipper", "VectorClipper", "SlidingWindowSampler", "VectorSampler"]
+__all__ = ["RasterClipper", "VectorClipper", "SamplingWindowGenerator", "BaseSampler", "Sampler"]
