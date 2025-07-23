c62853a17ce5487806653e6c0018ccb284a26dbf
Salar Ghaffarian <salarghaffarian1363@gmail.com>
2025-07-23 03:20:52 -0600

    Update the setup.py - change the package name from clipper to sampler. - change the address name to salar.ghaffarian1@gmail.com

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="clipper",
+    name="sampler",
     version="0.1",
     description="A package for raster and vector clipping.",
     author="Salar Ghaffarian",
-    author_email="salar@myheat.ca",
+    author_email="salar.ghaffarian1@gmail.com",
     packages=find_packages(),
     install_requires=[ "gdal","numpy"],
     python_requires=">=3.7", 
