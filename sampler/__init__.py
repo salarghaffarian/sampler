4e50ead66a2ad36febd99366990203487954cc28
Salar Ghaffarian <salarghaffarian1363@gmail.com>
2025-07-27 17:05:29 -0600

    Add the version

--- sampler/__init__.py
+++ sampler/__init__.py
@@ -2,5 +2,7 @@
 from .clippers import RasterClipper, VectorClipper
 from .samplers import SamplingWindowGenerator, BaseSampler, Sampler
 
+
 # Define the __all__ variable to let them imported directly from the package.
 __all__ = ["RasterClipper", "VectorClipper", "SamplingWindowGenerator", "BaseSampler", "Sampler"]
+__version__ = "1.0.0"
